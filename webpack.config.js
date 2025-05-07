9d11e906e5379e1e1b680d27aa8a54a565d1c1ae
martinDTV <martin.delatorre@monterrey.gob.mx>
2025-05-06 18:47:46 -0600

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'images', to: 'images' },
+            ],
+        }),
+    ],
+    mode: 'production',
+};
